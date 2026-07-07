beb45e9a9418bae82e0bf9129919700dfbf61d84
vedansh <vedanshsaini7719@gmail.com>
2026-07-07 13:19:20 +0530

    changed port, env templates, lock file removed

--- gateway/run.py
+++ gateway/run.py
@@ -1,5 +1,7 @@
 # Copyright 2017-present, The Visdom Authors
+import os
 import uvicorn
 
 if __name__ == '__main__':
-    uvicorn.run('app.main:app', host='0.0.0.0', port=8000, reload=True)
+    port = int(os.getenv("PORT", 8082))
+    uvicorn.run('app.main:app', host='0.0.0.0', port=port, reload=True)
